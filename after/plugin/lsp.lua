47a9a98e336b4d95912776109931acca03a098dc
Jake Sheehan <jake.sheehan.dev@gmail.com>
2024-08-23 11:06:41 -0400

    init

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,29 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+-- don't add this function in the `on_attach` callback.
+-- `format_on_save` should run only once, before the language servers are active.
+lsp_zero.format_on_save({
+  format_opts = {
+    async = false,
+    timeout_ms = 10000,
+  },
+  servers = {
+    ['tsserver'] = {'javascript', 'typescript'},
+    ['rust_analyzer'] = {'rust'},
+    ['cssls'] = {'css'},
+  }
+})
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  },
+})
+
--- init.lua
+++ init.lua
@@ -1,44 +1,83 @@
 require("remaps")
+require("set")
 
 -- lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
 		-- or                              , branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim' }
+		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	{
-  		"neanias/everforest-nvim",
-  		version = false,
-  		lazy = false,
-  		priority = 1000, -- make sure to load this before all the other start plugins
-  		-- Optional; default configuration will be used if setup isn't called.
-  		config = function()
-    		require("everforest").setup({
-			background = "medium",
-    			transparent_background_level = 1,
-    			italics = true,
-    			disable_italic_comments = false,
-    		})
-  		end,
+		"neanias/everforest-nvim",
+		version = false,
+		lazy = false,
+		priority = 1000, -- make sure to load this before all the other start plugins
+		-- Optional; default configuration will be used if setup isn't called.
+		config = function()
+			require("everforest").setup({
+				background = "medium",
+				transparent_background_level = 1,
+				italics = true,
+				disable_italic_comments = false,
+			})
+		end,
 	},
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-	{"mbbill/undotree"},
-	{"tpope/vim-fugitive"},
+	{
+		"NoahTheDuke/vim-just",
+		ft = { "just" },
+	},
+	{
+		"IndianBoy42/tree-sitter-just",
+	},
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+	{ "MunifTanjim/prettier.nvim" },
+	{ "mbbill/undotree" },
+	{ "tpope/vim-fugitive" },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+	},
+	{
+		{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+		{ "williamboman/mason.nvim" },
+		{ "williamboman/mason-lspconfig.nvim" },
+		{ "neovim/nvim-lspconfig" },
+		{ "windwp/nvim-ts-autotag" },
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "hrsh7th/nvim-cmp" },
+		{ "L3MON4D3/LuaSnip" },
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equalent to setup({}) function
+	},
 })
 
 vim.cmd([[colorscheme everforest]])
-
